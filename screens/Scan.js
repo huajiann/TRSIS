e2906256198cadc56788c03a1984e0588e7eabef
huajiann <huajianchan0206@gmail.com>
2021-10-13 15:45:05 +0800

    added qr scanner

--- screens/Scan.js
+++ screens/Scan.js
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 const Scan = ({ navigation }) => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState("Not yet scanned!");
+
+  const askForCameraPemission = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status == "granted");
+    })();
+  };
+
+  useEffect(() => {
+    askForCameraPemission();
+  }, []);
+
+  // What happened when we scan the qrcode
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data);
+    console.log("Type : " + type + "\nData : " + data);
+  };
+
+  // Check permissions
+  if (hasPermission === null) {
+    return (
+      <View style={styles.container}>
+        <Text>Requesting for camera permission</Text>
+      </View>
+    );
+  }
+
+  // Check permissions
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ margin: 10 }}>No access to the camera</Text>
+        <Button title={"Allow Camera"} onPress={() => askForCameraPemission()} />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>This is the Scan Screen!</Text>
-      <Button title="Click Here" onPress={() => alert("Button Clicked!")} />
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={{ height: 400, width: 400 }}
+        />
+      </View>
+      <Text style={styles.maintext}>{text}</Text>
+
+      {scanned && <Button title={"Scan again?"} onPress={() => setScanned(false)} color="blue" />}
     </View>
   );
 };
@@ -18,5 +67,20 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "aquamarine",
+  },
+
+  barcodebox: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 300,
+    width: 300,
+    overflow: "hidden",
+    borderRadius: 30,
+    backgroundColor: "aquamarine",
+  },
+  maintext: {
+    fontSize: 16,
+    margin: 20,
   },
 });
